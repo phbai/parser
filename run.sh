223f2d5fc3e401f5e3c8917370b4f9cafc0c0ab9
phbai <48466846@qq.com>
2018-03-01 21:39:37 +0800

    修改启动脚本

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-docker run --name some-parser --link some-redis:redis -d -p 8000:8000 phbai/parser
-docker run -i -t --rm --name some-parser -p 8000:8000 --link some-redis:redis parser /bin/bash
+docker-compose up -d
